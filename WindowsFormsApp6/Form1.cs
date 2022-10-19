b0848f4dd00e19d0ae4f7ff290f0fa079e754e81
oleg4 <oleg4@LAPTOP-MSQS2C0F>
2022-10-19 19:03:50 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="" || comboBox1.SelectedIndex ==-1)
+                {
+                    MessageBox.Show(
+                        "поле не заполненно",
+                        "ошибка",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Error,
+                        MessageBoxDefaultButton.Button1,
+                        MessageBoxOptions.DefaultDesktopOnly);
+                }
+            label1.Text += comboBox1.SelectedItem.ToString()+" "+textBox1.Text+"\n";
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            StreamWriter sw = new StreamWriter("C:\\Users\\oleg4\\OneDrive\\Desktop\\papka\\g.txt", true) ;
+            
+            sw.WriteLine(label1.Text);
+            sw.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
